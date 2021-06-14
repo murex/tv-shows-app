9bd01ba17904d8b5656a379bdd35f48e3df827c7
Nuhal <rifai.nuhal@gmail.com>
2021-06-14 15:58:38 +0300

    adding 5-tv-shows-show-card-component increment

---
+++ 5-tv-shows-show-card-component/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+/*******************************************************************************
+* Copyright (c) {2003-2021} Murex S.A.S. and its affiliates.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the MIT License
+* which accompanies this distribution, and is available at
+* https://opensource.org/licenses/MIT
+*******************************************************************************/
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
